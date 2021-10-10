c022b8bbb1128bb3d51d9fd09156f5a613045ac3
Derek Tan <derektan@berkeley.edu>
2021-10-09 17:09:57 -0700

    done with part a

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -26,33 +26,41 @@ const Total = (props) => (
   <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
 )
 
+const Course = ({course}) => {
+  return(
+    <div>
+      <Header course={course.name} />
+      <ul>
+        {course.parts.map(part => <Part key={part.id} part={part.name} num={part.exercises}/>)}
+      </ul>
+    </div>
+  )
+}
+
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
-
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
